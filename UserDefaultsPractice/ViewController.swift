c63d479af0e0d4152dac612534b7958f4133ac69
sudeepa88 <palsudeepa@gmail.com>
2023-12-22 08:45:20 +0530

    Initial Commit

---
+++ UserDefaultsPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserDefaultsPractice
+//
+//  Created by Sudeepa Pal on 22/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
